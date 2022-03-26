f7887e77c503adf918b74e7f16e3188b7a47a780
aneeb iqbal <1aneebiqbal@gmail.com>
2022-03-26 14:09:28 +0500

    RoboFriends-UI

---
+++ src/CardList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Crad from "./Card";
+
+const CardList = ({robots , a, b}) => {
+  const cardArray = robots.map((user , i) =>{
+      return(
+      <Crad 
+      key={i}
+      
+       id={robots[i].id}
+        name={robots[i].name}
+         email={robots[i].email}
+    />
+      );
+     })
+  return(
+    <div>
+    {cardArray}
+    </div>
+  );
+}
+export default CardList;
---
+++ src/Robots.js
@@ -0,0 +1,56 @@
+export const robots =[
+  {
+    id:1,
+    name: 'aneeb iqbal',
+    username: 'aneeb',
+    email: '1aneebiqbal@gmail.com'
+  },
+  {
+    id:2,
+    name: 'hassan saulat',
+    username: 'hassan',
+    email: 'hassansaulat@gmail.com'
+  },
+  {
+    id:2.5,
+    name: 'Zaira Iqbal',
+    username: 'Zaira.IQ',
+    email: 'Zairaiqbal@gmail.com'
+  },
+  {
+    id:3,
+    name: 'fazeel aslam',
+    username: 'fazeel',
+    email: 'fazeel@gmail.com'
+  },
+  {
+    id:4,
+    name: 'umer baadar',
+    username: 'baadar',
+    email: 'umer@gmail.com'
+  },
+  {
+    id:5,
+    name: 'sohaib ahmad',
+    username: 'sohaib',
+    email: 'sohaib@gmail.com'
+  },
+  {
+    id:6,
+    name: ' mia sohail',
+    username: 'sohail',
+    email: 'sohail@gmail.com'
+  },
+  {
+    id:7,
+    name: 'shahjahan saeed',
+    username: 'shahjhan',
+    email: 'shahjhan@gmail.com'
+  },
+  {
+    id:8,
+    name: 'Sumbal Imama',
+    username: 'sumbal',
+    email: 'sumbal@gmail.com'
+  }
+]
