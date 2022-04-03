9c8bd65ffd2692487c121f0ef2bb0b7cf87eb723
aneeb iqbal <1aneebiqbal@gmail.com>
2022-04-03 15:45:41 +0500

    Completed hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState, useEffect } from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import 'tachyons';
@@ -6,46 +6,49 @@ import Scroll from './Scroll';
 import { robots } from "./Robots";
 import { render } from "react-dom";
 
-const state = {
-  
-}
+function App () {
+  // constructor(){
+  //   super()
+  //   this.state ={
+  //     robots: [],
+  //     searchfield: ''
+  //   }
+  // }
+  const [robots, setRobots] = useState([])
+  const [searchfield , setSearchfield] = useState('')
+  const [count , setCount ]= useState(0)
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state ={
-      robots: [],
-      searchfield: ''
-    }
-  }
-componentDidMount(){
+useEffect(()=>{
   fetch('https://jsonplaceholder.typicode.com/users')
   .then (Response =>Response.json())
-  .then (users =>this.setState({robots:users}));
-}
-  onSearchChange =(event) =>{
-    this.setState({searchfield: event.target.value})
+  .then (users =>setRobots(users));
+  console.log(count)
+},[count])
+  const onSearchChange =(event) => {
+    setSearchfield(event.target.value)
   }
-  render(){
-    const filterRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+ 
+  {
+    const filterRobots = robots.filter(robots => {
+      return robots.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if(this.state.robots.length===0){
+    console.log(robots,searchfield)
+    if(robots.length===0){
       return <h1 className="tc">/LOADING/</h1>
     }
     else{
       return(
         <div className="tc">
         <h1>ROBO FRIENDS</h1>
-        <SearchBox searchChange ={this.onSearchChange} />
+        <button onClick={()=>setCount(count+1)}>ClickMe!!</button>
+        <SearchBox searchChange ={onSearchChange} />
         <Scroll>
         <CardList robots ={filterRobots}/>
         </Scroll>
         </div>
       );
     }
-    
-  }
+  } 
   
 }
 export default App;
